88a8c4decb147ba09fe8737011547b62d377fb6d
Angus Hopkinson <angushopkinson@gmail.com>
2022-08-16 10:34:15 +0530

    Added token symbols

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import * as React from 'react';
 import { ThemeProvider } from '@mui/material/styles';
 import { theme } from './theme';
-import { Card, CardContent, CssBaseline, InputAdornment, Paper, Slider, TextField } from '@mui/material';
+import { Avatar, AvatarGroup, Card, CardContent, CssBaseline, InputAdornment, Paper, Slider, Stack, TextField } from '@mui/material';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import MuiInput from '@mui/material/Input';
 import { Legend, Line, LineChart, ReferenceLine, Tooltip, XAxis, YAxis } from "recharts";
 import MathJax from 'react-mathjax';
+
+import logoUSDC from './images/USDC.png';
+import logoAVAX from './images/Avalanche_AVAX_RedWhite.png';
+import logoYyAVAX from './images/yyAVAX.jpg';
 
 // This function is used in order to make recharts charts responsive, by detecting the size of
 // the component in which they find themselves. Using this because recharts own ResponsiveContainer
@@ -33,6 +37,29 @@ const useResize = (myRef) => {
 
   return { width }
 };
+
+const TokenSymbols = (props) => {
+  const { number, token0, altToken0, token1, altToken1, token2 = token0, altToken2 = altToken0, token3 = token1, altToken3 = altToken1 } = props;
+  return (
+    <Stack direction="row" alignItems="center" spacing={1}>
+      <Typography variant="h5">
+        {number}.
+      </Typography>
+      <AvatarGroup>
+        <Avatar alt={altToken0} src={token0} />
+        <Avatar alt={altToken1} src={token1} />
+      </AvatarGroup>
+      <Typography variant="h5">
+        vs.
+      </Typography>
+      <Avatar alt={altToken2} src={token2} />
+      <Typography variant="body1">
+        &
+      </Typography>
+      <Avatar alt={altToken3} src={token3} />
+    </Stack>
+  )
+}
 
 function App() {
   const [yyAvaxGrowth, setYyAvaxGrowth] = React.useState(0.09);
@@ -91,23 +118,68 @@ function App() {
                   <Typography variant="h6">
                     How it works
                   </Typography>
+                </Grid>
+                <Grid item xs={12}>
                   <Typography variant="body1">
                     We show 3 charts in the graph below:
                   </Typography>
+                </Grid>
+                <Grid item xs={12} container alignItems="center" spacing={2}>
+                  <Grid item xs="auto">
+                    <TokenSymbols
+                      number={1}
+                      token0={logoAVAX}
+                      altToken0="AVAX"
+                      token1={logoUSDC}
+                      altToken1="$"
+                    />
+                  </Grid>
+                  <Grid item xs>
+                    <Typography variant="body1">
+                      <span style={{ color: theme.palette.avax.main }}>AVAX/USDC vs. holding AVAX and USDC</span> - this is the "normal" impermanent
+                      loss calculation for a given variation in the price of AVAX when depositing into an AVAX/USDC pool.
+                    </Typography>
+                  </Grid>
+                </Grid>
+                <Grid item xs={12} container alignItems="center" spacing={2}>
+                  <Grid item xs="auto">
+                    <TokenSymbols
+                      number={2}
+                      token0={logoYyAVAX}
+                      altToken0="yyAVAX"
+                      token1={logoUSDC}
+                      altToken1="$"
+                    />
+                  </Grid>
+                  <Grid item xs>
+                    <Typography variant="body1">
+                      <span style={{ color: theme.palette.info.main }}>yyAVAX/USDC vs. holding yyAVAX and USDC</span> - this is also the "normal" impermanent
+                      loss calculation, but we have separated out the effect of yyAVAX accruing value against AVAX, and is for depositing into a yyAVAX/USDC pool.
+                    </Typography>
+                  </Grid>
+                </Grid>
+                <Grid item xs={12} container alignItems="center" spacing={2}>
+                  <Grid item xs="auto">
+                    <TokenSymbols
+                      number={3}
+                      token0={logoYyAVAX}
+                      altToken0="yyAVAX"
+                      token1={logoUSDC}
+                      altToken1="$"
+                      token2={logoAVAX}
+                      altToken2="AVAX"
+                    />
+                  </Grid>
+                  <Grid item xs>
                   <Typography variant="body1">
-                    1. <span style={{ color: theme.palette.avax.main }}>AVAX/USDC vs. holding AVAX and USDC</span> - this is the "normal" impermanent
-                    loss calculation for a given variation in the price of AVAX when depositing into an AVAX/USDC pool.
-                  </Typography>
-                  <Typography variant="body1">
-                    2. <span style={{ color: theme.palette.info.main }}>yyAVAX/USDC vs. holding yyAVAX and USDC</span> - this is also the "normal" impermanent
-                    loss calculation, but we have separated out the effect of yyAVAX accruing value against AVAX, and is for depositing into a yyAVAX/USDC pool.
-                  </Typography>
-                  <Typography variant="body1">
-                    3. <span style={{ color: theme.palette.primary.main }}>yyAVAX/USDC vs. holding AVAX and USDC</span> - this is not a normal impermanent loss
+                    <span style={{ color: theme.palette.primary.main }}>yyAVAX/USDC vs. holding AVAX and USDC</span> - this is not a normal impermanent loss
                     calculation because we are comparing the pooled yyAVAX/USDC assets against the plain AVAX and USDC assets. The comparison being made in this
                     case is what you stand to lose {"("}or gain{")"} for the case that you hold your AVAX and USDC assets versus converting the AVAX to yyAVAX
                     and then depositing into a yyAVAX/USDC pool.
                   </Typography>
+                  </Grid>
+                </Grid>
+                <Grid item xs={12}>
                   <br />
                   <Typography variant="body1">
                     The slider can be used to adjust the expected gains you receive from yyAVAX accruing value against AVAX.
@@ -209,7 +281,7 @@ function App() {
                       <Line type="monotone" dot={false} dataKey="yyAVAX/USDC pool vs. holding AVAX and USDC" stroke={theme.palette.primary.main} />
                       <ReferenceLine y={0} stroke={theme.palette.text.disabled} strokeDasharray="3 3" />
                       <XAxis dataKey="New AVAX Price" type="number" domain={[0, avaxPrice * 4]} tickCount={5} tickFormatter={v => `$${v.toFixed(0)}`} label={{ value: "New AVAX price", fill: theme.palette.text.primary, dy: 15 }} />
-                      <YAxis orientation="left" domain={[-1, 0.5]} ticks={[-1, -0.75, -0.5, -0.25, 0, 0.25, 0.5]} tickFormatter={v => `${100 * v}%`} label={{ value: "Net loss/gain from IL & other gains", fill: theme.palette.text.primary, angle: -90, dx: -30 }} />
+                      <YAxis orientation="left" domain={[-1, 0.5]} ticks={[-1, -0.75, -0.5, -0.25, 0, 0.25, 0.5]} tickFormatter={v => `${100 * v}%`} label={{ value: "Net loss/gain from IL & other growth", fill: theme.palette.text.primary, angle: -90, dx: -30 }} />
                       <Tooltip formatter={v => `${(100 * v).toFixed(2)}%`} labelFormatter={label => `New price = $${label.toFixed(2)} per AVAX, (Old price = $${avaxPrice.toFixed(2)})`} contentStyle={{ backgroundColor: theme.palette.background.default, opacity: 0.75 }} labelStyle={{ color: theme.palette.text.primary }} />
                       <Legend verticalAlign="top" iconSize="8" />
                     </LineChart>
@@ -274,8 +346,8 @@ function App() {
             </Paper>
           </Grid>
         </Grid>
-      </Container>
-    </ThemeProvider>
+      </Container >
+    </ThemeProvider >
   );
 }
 
